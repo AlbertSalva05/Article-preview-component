5c4f58a233b1e8c6626f3788c047b61da8400ad3
AlbertSalva05 <albertsalva360@gmail.com>
2023-10-03 22:45:58 +0800

    update js 231003_02

--- root/js/main.js
+++ root/js/main.js
@@ -3,7 +3,6 @@ $(function(){
   // declare variables
   var responsive_viewport = $(window).width();
 
-
   // trigger function on shareBtn
   $(".shareBtn").click(function(){
     // toggle class on shareBtn on click
@@ -11,9 +10,19 @@ $(function(){
     // toggle class on shareContent
     $(".shareContent").toggleClass("show");
 
-    // check if viewport is SP
-    if (responsive_viewport < 768) {
-      $(".articleContent").toggleClass("share_content_active");
+    // // check if viewport is SP
+    function checkIfMobile() {
+      if (responsive_viewport < 768) {
+        $(".articleContent").toggleClass("share_content_active");
+      }
     }
+
+    // run function on load and on resize
+    $(window).on('load resize', function(){
+      checkIfMobile();
+    });
+
+    $(".articleContent").toggleClass("share_content_active");
+
   });
 });
