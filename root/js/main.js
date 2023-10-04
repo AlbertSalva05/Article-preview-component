b36447d0f855cc4254255c9addf4c6d0c743fb6e
AlbertSalva05 <albertsalva360@gmail.com>
2023-10-04 13:23:20 +0800

    update style and js 231004_01

--- root/js/main.js
+++ root/js/main.js
@@ -1,28 +1,27 @@
 $(function(){
 
-  // declare variables
-  var responsive_viewport = $(window).width();
+	// declare variables
+	var responsive_viewport = $(window).width();
 
-  // trigger function on shareBtn
-  $(".shareBtn").click(function(){
-    // toggle class on shareBtn on click
-    $(this).toggleClass("share_active");
-    // toggle class on shareContent
-    $(".shareContent").toggleClass("show");
+	// trigger function on shareBtn
+	$(".shareBtn").click(function(){
+		// toggle class on shareBtn/shareContent on click
+		$(this).toggleClass("share_active");
+		$(".shareContent").toggleClass("show");
 
-    // // check if viewport is SP
-    function checkIfMobile() {
-      if (responsive_viewport < 768) {
-        $(".articleContent").toggleClass("share_content_active");
-      }
-    }
+		// // check if viewport is SP
+		function checkIfMobile() {
+			if (responsive_viewport < 768) {
+				$(".articleContent").toggleClass("share_content_active");
+			}
+		}
 
-    // run function on load and on resize
-    $(window).on('load resize', function(){
-      checkIfMobile();
-    });
+		// run function on load and on resize
+		$(window).on('load resize', function(){
+			checkIfMobile();
+		});
 
-    $(".articleContent").toggleClass("share_content_active");
+		$(".articleContent").toggleClass("share_content_active");
 
-  });
+	});
 });
